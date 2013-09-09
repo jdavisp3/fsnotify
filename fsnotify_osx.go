0eb825c50989ba9bbbfd5c096296fe2b9ecb6f63
Sam Jacobson <sam@eversoft.co.nz>
2013-09-09 19:08:35 +1200

    removed old cruft from previous interface; changed implementation to create one stream per watch. partial support for new API

--- fsnotify_osx.go
+++ fsnotify_osx.go
@@ -17,10 +17,6 @@ static CFArrayRef ArrayCreateMutable(int len) {
 
 extern void fsevtCallback(FSEventStreamRef p0, void * info, size_t p1, char** p2, FSEventStreamEventFlags* p3, FSEventStreamEventId* p4);
 
-static FSEventStreamRef EventStreamCreateRelativeToDevice(FSEventStreamContext * context, dev_t dev, CFArrayRef paths, FSEventStreamEventId since, CFTimeInterval latency) {
-	return FSEventStreamCreateRelativeToDevice(NULL, (FSEventStreamCallback) fsevtCallback, context, dev, paths, since, latency, 0);
-}
-
 static FSEventStreamRef EventStreamCreate(FSEventStreamContext * context, CFArrayRef paths, FSEventStreamEventId since, CFTimeInterval latency) {
 	return FSEventStreamCreate(NULL, (FSEventStreamCallback) fsevtCallback, context, paths, since, latency, 0);
 }
@@ -31,6 +27,7 @@ import "unsafe"
 import "path/filepath"
 import "sync"
 import "os"
+import "time"
 
 
 
@@ -77,103 +74,6 @@ func fsevtCallback(stream C.FSEventStreamRef, info unsafe.Pointer, numEvents C.s
 	}
 }
 
-/*
-	extern FSEventStreamRef FSEventStreamCreate(
-		CFAllocatorRef allocator,
-		FSEventStreamCallback callback,
-		FSEventStreamContext *context,
-		CFArrayRef pathsToWatch,
-		FSEventStreamEventId sinceWhen,
-		CFTimeInterval latency,
-		FSEventStreamCreateFlags flags);
-
-	typedef void ( *FSEventStreamCallback )(
-		ConstFSEventStreamRef streamRef,
-		void *clientCallBackInfo,
-		size_t numEvents,
-		void *eventPaths,
-		const FSEventStreamEventFlags eventFlags[],
-		const FSEventStreamEventId eventIds[]);
-*/
-
-func FSEventsLatestId() uint64 {
-	return uint64(C.FSEventsGetCurrentEventId())
-}
-
-func DeviceForPath(pth string) int64 {
-	cStat := C.struct_stat{}
-	cPath := C.CString(pth)
-	defer C.free(unsafe.Pointer(cPath))
-
-	_ = C.lstat(cPath, &cStat)
-	return int64(cStat.st_dev)
-}
-
-func GetIdForDeviceBeforeTime(dev, tm int64) uint64 {
-	return uint64(C.FSEventsGetLastEventIdForDeviceBeforeTime(C.dev_t(dev), C.CFAbsoluteTime(tm)))
-}
-
-func FSEventsSince(paths []string, dev int64, since uint64) []FileEvent {
-	cPaths := C.ArrayCreateMutable(C.int(len(paths)))
-	defer C.CFRelease(C.CFTypeRef(cPaths))
-
-	for _, p := range paths {
-		p, _ = filepath.Abs(p)
-		cpath := C.CString(p)
-		defer C.free(unsafe.Pointer(cpath))
-
-		str := C.CFStringCreateWithCString(nil, cpath, C.kCFStringEncodingUTF8)
-		C.CFArrayAppendValue(cPaths, unsafe.Pointer(str))
-	}
-
-	if since == 0 {
-		/* If since == 0 is passed to FSEventStreamCreate it will mean 'since the beginning of time'.
-		We remap to 'now'. */
-		since = C.kFSEventStreamEventIdSinceNow + (1 << 64)
-	}
-
-	evtC := make(chan []FileEvent)
-	context := C.FSEventStreamContext{info: unsafe.Pointer(&evtC)}
-
-	latency := C.CFTimeInterval(1.0)
-	var stream C.FSEventStreamRef
-	if dev != 0 {
-		stream = C.EventStreamCreateRelativeToDevice(&context, C.dev_t(dev), cPaths, C.FSEventStreamEventId(since), latency)
-	} else {
-		stream = C.EventStreamCreate(&context, cPaths, C.FSEventStreamEventId(since), latency)
-	}
-
-	rlref := C.CFRunLoopGetCurrent()
-
-	go func() {
-		/* Schedule the stream on the runloop, then run the runloop concurrently with starting/flushing/stopping the stream */
-		C.FSEventStreamScheduleWithRunLoop(stream, rlref, C.kCFRunLoopDefaultMode)
-		go func() {
-			C.CFRunLoopRun()
-		}()
-		C.FSEventStreamStart(stream)
-		C.FSEventStreamFlushSync(stream)
-		C.FSEventStreamStop(stream)
-		C.FSEventStreamInvalidate(stream)
-		C.FSEventStreamRelease(stream)
-		C.CFRunLoopStop(rlref)
-		close(evtC)
-	}()
-
-	var events []FileEvent
-	for evts := range evtC {
-		events = append(events, evts...)
-	}
-
-	return events
-}
-
-type FSEventStream struct {
-	stream C.FSEventStreamRef
-	rlref  C.CFRunLoopRef
-	C      chan []FileEvent
-}
-
 type FileEvent struct {
 	Name  string // File name (optional)
 	flags uint32
@@ -197,10 +97,9 @@ func (e *FileEvent) IsRename() bool { return (e.flags & EventFlagItemRenamed) ==
 
 
 type Watcher struct {
-	stream C.FSEventStreamRef
 	rlref  C.CFRunLoopRef
 	mu            sync.Mutex          // Mutex for the Watcher itself.
-	watches       map[string]int      // Map of watched file diescriptors (key: path)
+	watches       map[string]C.FSEventStreamRef      // key: path; one entry per 'watch'. maps to stream that watches path
 	wmut          sync.Mutex          // Protects access to watches.
 	fsnFlags      map[string]uint32   // Map of watched files to flags used for filter
 	fsnmut        sync.Mutex          // Protects access to fsnFlags.
@@ -210,7 +109,7 @@ type Watcher struct {
 	finfo         map[int]os.FileInfo // Map of file information (isDir, isReg; key: watch descriptor)
 	pmut          sync.Mutex          // Protects access to paths and finfo.
 	fileExists    map[string]bool     // Keep track of if we know this file exists (to stop duplicate create events)
-	femut         sync.Mutex          // Proctects access to fileExists.
+	femut         sync.Mutex          // Protects access to fileExists.
 	Error         chan error          // Errors are sent on this channel
 	internalEvent chan *FileEvent     // Events are queued on this channel
 	Event         chan *FileEvent     // Events are returned on this channel
@@ -220,27 +119,9 @@ type Watcher struct {
 }
 
 
-func NewWatcher(paths []string) (*Watcher, error) {
-	cPaths := C.ArrayCreateMutable(C.int(len(paths)))
-	defer C.CFRelease(C.CFTypeRef(cPaths))
-
-	for _, p := range paths {
-		p, _ = filepath.Abs(p)
-		cpath := C.CString(p)
-		defer C.free(unsafe.Pointer(cpath))
-
-		str := C.CFStringCreateWithCString(nil, cpath, C.kCFStringEncodingUTF8)
-		C.CFArrayAppendValue(cPaths, unsafe.Pointer(str))
-	}
-
-//	if since == 0 {
-		/* If since == 0 is passed to FSEventStreamCreate it will mean 'since the beginning of time'.
-		We remap to 'now'. */
-		since := C.kFSEventStreamEventIdSinceNow
-//	}
-
+func NewWatcher() (*Watcher, error) {
 	w := &Watcher{
-		watches:       make(map[string]int),
+		watches:       make(map[string]C.FSEventStreamRef),
 		fsnFlags:      make(map[string]uint32),
 		enFlags:       make(map[string]uint32),
 		paths:         make(map[int]string),
@@ -251,41 +132,104 @@ func NewWatcher(paths []string) (*Watcher, error) {
 		Error:         make(chan error),
 		done:          make(chan bool, 1),
 	}
-	context := C.FSEventStreamContext{info: unsafe.Pointer(&w.internalEvent)}
 
-	latency := C.CFTimeInterval(1.0)
-	w.stream = C.EventStreamCreate(&context, cPaths, C.FSEventStreamEventId(since), latency)
+	w.rlref = C.CFRunLoopGetCurrent()		// XXX: accessing the 'current' runloop. This will cause problems with multiple watchers in one process
 
-	w.rlref = C.CFRunLoopGetCurrent()
-
+	// Start the runloop. This exists for the duration of the watcher
 	go func() {
-		/* Schedule the stream on the runloop, then run the runloop concurrently with starting/flushing/stopping the stream */
-		C.FSEventStreamScheduleWithRunLoop(w.stream, w.rlref, C.kCFRunLoopDefaultMode)
-		go func() {
-			C.CFRunLoopRun()
-		}()
-		C.FSEventStreamStart(w.stream)
+		C.CFRunLoopRun()
 	}()
 
 	return w, nil
 }
 
 func (w *Watcher) watch(path string) error {
+	return w.watchPath(path, nil)
+}
+
+type Options struct {
+    Recursive        bool
+    WatchHidden      bool
+    Pattern          string
+    Throttle         bool
+    ThrottleDuration time.Duration
+//    Triggers         Triggers
+    Verbose          bool
+    // contains filtered or unexported fields
+}
+
+func (w *Watcher) watchPath(path string, options *Options) error {
+	path, _ = filepath.Abs(path)
+
+	w.wmut.Lock()
+	_, found := w.watches[path]
+	w.wmut.Unlock()
+
+	if !found {
+		cPaths := C.ArrayCreateMutable(C.int(1))
+		defer C.CFRelease(C.CFTypeRef(cPaths))
+
+		cpath := C.CString(path)
+		defer C.free(unsafe.Pointer(cpath))
+
+		str := C.CFStringCreateWithCString(nil, cpath, C.kCFStringEncodingUTF8)
+		C.CFArrayAppendValue(cPaths, unsafe.Pointer(str))
+
+		context := C.FSEventStreamContext{info: unsafe.Pointer(&w.internalEvent)}
+		latency := C.CFTimeInterval(0)
+		if options != nil && options.Throttle {
+			latency = C.CFTimeInterval(options.ThrottleDuration / time.Second)
+		}
+		stream := C.EventStreamCreate(&context, cPaths, C.kFSEventStreamEventIdSinceNow + (1 << 64), latency)
+		w.wmut.Lock()
+		w.watches[path] = stream
+		w.wmut.Unlock()
+		C.FSEventStreamScheduleWithRunLoop(stream, w.rlref, C.kCFRunLoopDefaultMode)
+		C.FSEventStreamStart(stream)
+	}
+
 	return nil;
 }
 
 func (w *Watcher) removeWatch(path string) error {
+	w.wmut.Lock()
+	stream, found := w.watches[path]
+	w.wmut.Unlock()
+
+	if found {
+		C.FSEventStreamStop(stream)
+		C.FSEventStreamInvalidate(stream)
+		C.FSEventStreamRelease(stream)
+		w.wmut.Lock()
+		delete(w.watches, path)
+		w.wmut.Unlock()
+	}
+
 	return nil;
 }
 
-/*func (es *FSEventStream) Flush() {
-	C.FSEventStreamFlushSync(es.stream)
+// Close closes a watcher instance
+// It sends a message to the reader goroutine to quit and removes all watches
+// associated with the kevent instance
+func (w *Watcher) Close() error {
+	w.mu.Lock()
+	if w.isClosed {
+		w.mu.Unlock()
+		return nil
+	}
+	w.isClosed = true
+	w.mu.Unlock()
+
+	// Send "quit" message to the reader goroutine
+	w.done <- true
+	w.wmut.Lock()		// I think fsnotify_bsd may have a very minor bug? It used w.pmut
+	ws := w.watches
+	w.wmut.Unlock()
+	for path := range ws {
+		w.removeWatch(path)
+	}
+	C.CFRunLoopStop(w.rlref)
+
+	return nil
 }
 
-func (es *FSEventStream) Stop() {
-	C.FSEventStreamStop(es.stream)
-	C.FSEventStreamInvalidate(es.stream)
-	C.FSEventStreamRelease(es.stream)
-	C.CFRunLoopStop(es.rlref)
-	close(es.C)
-}*/
